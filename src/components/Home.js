fe828b6214dc7ea31e94c03379f28c12985c5757
Aryan <aryannnaik@outlook.com>
2021-08-14 13:49:54 -0400

    grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ import API from "../API";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
-
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 // Image
@@ -17,7 +18,22 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
